dcd35a2ae6cae61039e2b2997cb7e022ca7c16a6
王绍楠 <wangsn666@163.com>
2024-12-09 16:04:19 +0800

    chore: upgrade dependencies

---
+++ test-code/switch.js
@@ -0,0 +1,12 @@
+function handle(tag) {
+  switch (tag) {
+    case 1:
+      console.log("1");
+    case 2:
+      console.log("2");
+    default:
+      console.log("default");
+  }
+}
+
+handle(1);
