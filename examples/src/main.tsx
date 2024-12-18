994b2c0dd88d2dabdd22528a4fa3993e3f99c679
王绍楠 <wangsn666@163.com>
2024-12-18 18:57:44 +0800

    chore: update build config

--- examples/src/main.tsx
+++
@@ -1,8 +0,0 @@
-// import { createRoot } from "react-dom/client";
-import "./index.css";
-import * as React from "../../dist/node_modules/react/index.js";
-
-const App = <div>react</div>;
-
-console.log("[ App ] >", App);
-// createRoot(document.getElementById("root")!).render(<App />);
---
+++ examples/test-fc/main.tsx
@@ -0,0 +1,15 @@
+// import ReactDOM from "react-dom";
+
+function App() {
+  return <div>react</div>;
+}
+
+console.log("[ App ] >", <App />);
+
+// function Child({ children }) {
+//   return <li>{children}</li>;
+// }
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+// root.render(<App />);
