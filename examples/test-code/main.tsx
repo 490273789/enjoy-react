509e4d9cda095685461f8a7ac662dba44f8fd3eb
王绍楠 <wangsn666@163.com>
2024-12-20 10:51:34 +0800

    feat: update test code

---
+++ examples/test-code/main.tsx
@@ -0,0 +1,21 @@
+// import ReactDOM from "react-dom";
+
+// function App() {
+//   return <div>react</div>;
+// }
+
+const element = (
+  <div key="1" className="list-wrapper">
+    hello <span className="item">react</span>
+  </div>
+);
+console.log("[ element ] >", element);
+// console.log("[ App ] >", <App key={1} />);
+
+// function Child({ children }) {
+//   return <li>{children}</li>;
+// }
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+// root.render(<App />);
